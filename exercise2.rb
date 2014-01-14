f638d9369eda11d6aad7f033a2f9b2ec3248c55a
nizarith <nizarith@live.com>
2014-01-13 23:08:38 -0330

    logic

--- exercise2.rb
+++ exercise2.rb
@@ -6,3 +6,12 @@ puts "Your tip for this meal is #{cost}"
 
 math = 45628 * 7839
 puts "45628 multiplied by 7839 is #{math}"
+
+
+arg1 = true && false
+arg2 = false && true
+arg3 = !(false && false)
+
+puts arg1
+puts arg2
+puts arg3
