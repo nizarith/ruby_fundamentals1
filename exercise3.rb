26f185231cc7d5cb9a614f31de1c98b4c19c83da
nizarith <nizarith@live.com>
2014-01-13 23:36:01 -0330

    calc year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,12 @@ puts "What is your name?"
 
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "In the form of a number how old are you #{name}?"
+age = gets.chomp
+year = 2014 - Integer(age)
+year1 = Integer(year) - 1
+	puts "Wow #{name} you were born in either #{year} or #{year1}?"
+
+# improve accuracy with month + date"
+# fix to make sure it is integer
