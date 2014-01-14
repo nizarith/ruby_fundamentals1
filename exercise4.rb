07f79a627e24bf2a967fe05f44d5021a0abce275
nizarith <nizarith@live.com>
2014-01-14 15:09:04 -0330

    Update exercise4.rb

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,11 @@
 1.upto(100) do |i|
 
-	if i % 3 == 0
-		puts "Bit"
+	if i % 3 == 0 and i % 5 == 0
+		puts "Bitmaker"
 	elsif i % 5 == 0
 		puts "Maker"
-	elsif i % 3 == 0 and i % 5 == 0
-		puts "Bitmaker"
+	elsif i % 3 == 0
+		puts "Bit"
 	else
 		puts i
 	end
